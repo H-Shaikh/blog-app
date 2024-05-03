0b76995471a96b61b8e939b80efcb389797f2ffe
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2024-05-03 09:59:00 +0530

    tags using djang-taggit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,20 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    
     # pagination with 3 post per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -29,11 +36,20 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                    .order_by('-same_tags','-publish')[:4]
+    
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
